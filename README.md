81a2017b3f1038a98cd1473a6fc0bc28278c4642
joneshf <jones3.hardy@gmail.com>
2016-10-15 20:09:59 -0700

    Provide a pure tail recursive function

---
+++ README.md
@@ -0,0 +1,52 @@
+# elm-tail-recursion
+
+SUPRISE! elm has runtime exceptions!
+Don't let anybody tell you differently.
+Rather than pretend they don't exist,
+this library hopes to mitigate their occurrences.
+
+## Crash
+
+The most obvious offender is [`crash`][crash].
+I mean it says it right in the name.
+It also says it right in the type `String -> a`.
+Which means that given a string, you can produce ANY value in all of elm.
+Sounds remarkable like null...
+
+[`crash`][crash] is not magic, and so it can't do what it actually says.
+Otherwise, we could have all our implementations be one line with a bunch of types.
+
+```elm
+type Google
+  = ...
+
+google : Google
+google =
+  Debug.crash "Oh look, we made Google!"
+```
+
+What [`crash`][crash] actually does is throw a runtime exception.
+But, [`crash`][crash] is not the only way to throw a runtime exception.
+At least, there are other ways at the time of writing.
+
+## Tail Recursion
+
+A much sneaker way is to write a recursive function that isn't tail recursive and use it with certain arguments that cause js to blow the stack.
+It's sneaky in the fact that, we do this all the time, but rarely recognize it.
+Mostly, we get away with it most of the time because our input values usually keep the recursion bounded.
+But words like "mostly" and "usually" are indicators of hard to find bugs.
+You might also think js is to blame for this runtime exception.
+The fact of the matter is, elm generated the js that caused the runtime exception.
+
+Luckily, all is not lost!
+elm is smart enough to compile tail recursive functions into js that will not blow the stack.
+The solution is to always write tail recursive functions.
+
+We can help a bit by extracting the recursive part to a function that guarantees not to blow the stack.
+You may still run out of memory; but, that's a separate concern that this library doesn't handle.
+You also may still have unbounded recursion; but, again, that's a separate concern that this library doesn't handle.
+
+The takeaway is that recursion is not your enemy.
+Non-tail recursive recursion is your enemy—currently anyway.
+
+[crash]: http://package.elm-lang.org/packages/elm-lang/core/4.0.5/Debug#crash
---
+++ src/TailRecursion.elm
@@ -0,0 +1,205 @@
+module TailRecursion exposing (recurse)
+
+{-|
+# Table of Contents
+
+1. [Functions](#functions)
+1. [Example](#example)
+  1. [Mutual Recursion](#mutual-recursion)
+  1. [Direct Recursion](#direct-recursion)
+  1. [Using recurse](#using-recurse)
+  1. [Nota Bene](#nota-bene)
+  1. [Put it All Together](#put-it-all-together)
+
+## Functions
+@docs recurse
+
+## Example
+
+A simple example is in order.
+When writing mutually recursive functions,
+elm compiles to code that can blow the stack.
+Mutual recursion isn't the only place we can get a benefit here,
+so don't read too much into the example.
+
+### Mutual Recursion
+For instance, the following functions can blow the stack:
+
+```elm
+even : Int -> Bool
+even n =
+  case n of
+    0 ->
+      True
+    _ ->
+      odd (n - 1)
+
+odd : Int -> Bool
+odd n =
+  case n of
+    0 ->
+      False
+    _ ->
+      even (n - 1)
+```
+
+These functions are fine for small positive values.
+But if we apply a large `Int` (like `1000000`), elm will throw a runtime exception.
+
+This example is trivial and contrived for a reason.
+There are multiple ways to solve this problem better, but that's not the purpose of it.
+The purpose is to show that even a simple pair of mutually recursive functions can cause runtime errors with elm.
+
+### Direct Recursion
+
+We can write this tail recursively by just inlining the body of both mutual recursions.
+
+```elm
+even : Int -> Bool
+even n =
+  case n of
+    0 ->
+      True
+    _ ->
+      case (n - 1) of
+        0 ->
+          False
+        _ ->
+          even ((n - 1) - 1)
+
+odd : Int -> Bool
+odd n =
+  case n of
+    0 ->
+      False
+    _ ->
+      case (n - 1) of
+        0 ->
+          True
+        _ ->
+          odd ((n - 1) - 1)
+```
+
+After a bit of cleanup, it's even understandable!
+
+```elm
+even : Int -> Bool
+even n =
+  case n of
+    0 ->
+      True
+    1 ->
+      False
+    _ ->
+      even (n - 2)
+
+odd : Int -> Bool
+odd n =
+  case n of
+    0 ->
+      False
+    1 ->
+      True
+    _ ->
+      odd (n - 2)
+```
+
+A very clean and elegant solution, but a solution that removes the inherent mutual recursion of the original.
+Every problem we face with mutual recursion isn't always so easily changed.
+Perhaps even more important is that converting mutual recursion to direct recursion can't always happen.
+
+### Using [`recurse`][recurse]
+
+There's another solution we could have arrived at though.
+One that keeps the mutual recursion but protects ourselves from blowing the stack.
+If we extract the base and inductive cases out,
+we can use [`recurse`][recurse] instead.
+
+We need to supply a function `(a -> Result b a)` and `a` to [`recurse`][recurse].
+The `a` will be `Int`, as that's the input type of `even`/`odd`.
+The `b` will be `Bool`, as that's the return type of `even`/`odd`.
+
+So we need to create a function `Int -> Result Bool Int`.
+Let's look at `even` first.
+If the `Int` is `0`, we're done, we know what value to return—`True`.
+If the `Int` is anything else, we're not done.
+We need to recurse with the predecessor.
+
+Similar logic works for the `odd` step.
+
+```elm
+evenStep : Int -> Result Bool Int
+evenStep n =
+  case n of
+    0 ->
+      Err True
+    _ ->
+      Ok (n - 1)
+
+oddStep : Int -> Result Bool Int
+oddStep n =
+  case n of
+    0 ->
+      Err False
+    _ ->
+      Ok (n - 1)
+```
+
+### Nota Bene
+
+It's important to notice that we're not doing any actual recursion here.
+We're just converting `Int`s into `Result Bool Int`.
+I think most people will agree that it's much easier to understand and test non-recursive functions
+than recursive functions.
+
+### Put it All Together
+
+Now that we have our "recursive" functions, we need to put them to use with [`recurse`][recurse].
+For the `even` function, we want to follow the ideas of our original implementation.
+We first take an even step, and then we take an odd step.
+For the `odd` function, we first take an odd step, and then we take an even step
+The functions almost write themselves.
+
+```elm
+even : Int -> Bool
+even =
+  recurse <| \n ->
+    evenStep n `Result.andThen` oddStep
+
+odd : Int -> Bool
+odd =
+  recurse <| \n ->
+    oddStep n `Result.andThen` evenStep
+```
+
+Notice that we can lean on the our good friend `Result.andThen`.
+All of our functions that we know and use are still applicable here.
+We don't need to attempt to learn some new abstractions to use [`recurse`][recurse]
+
+Now if we apply `1000000` to `even`, we'll actually get a value, rather than a runtime exception!
+
+[recurse]: #recurse
+-}
+
+{-|
+If you have a non-tail recursive function,
+use this function to ensure it won't blow the stack.
+
+Some explanation for how this works.
+
+Thanks to purity and fast-and-loose reasoning,
+we have exactly one way to implement this function.
+
+The only way to get a `b` is to apply an `a` to `a -> Result a b`.
+If the result of application is `Ok a`,
+we still don't have a `b` and must apply this new `a` to `a -> Result a b`.
+At some point we get a value `Err b`, and thus we can finally end the recursion.
+-}
+recurse : (a -> Result b a) -> a -> b
+recurse f x =
+    case f x of
+        Ok x ->
+            recurse f x
+
+        Err y ->
+            y

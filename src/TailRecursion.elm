81f35bfa4df2ebd1183b664ff1b9c1576833b809
joneshf <jones3.hardy@gmail.com>
2016-10-16 07:14:58 -0700

    Add design explanation and simple usage example

--- src/TailRecursion.elm
+++ src/TailRecursion.elm
@@ -4,15 +4,63 @@ module TailRecursion exposing (recurse)
 # Table of Contents
 
 1. [Functions](#functions)
+1. [Design](#design)
+  1. [The `recurse` function](#the-recurse-recurse-function)
 1. [Example](#example)
   1. [Mutual Recursion](#mutual-recursion)
   1. [Direct Recursion](#direct-recursion)
-  1. [Using recurse](#using-recurse)
+  1. [Using `recurse`](#using-recurse-recurse-)
   1. [Nota Bene](#nota-bene)
   1. [Put it All Together](#put-it-all-together)
 
 ## Functions
 @docs recurse
+
+## Design
+
+### The [`recurse`][recurse] function
+
+Let's start with an explanation for how this function works.
+
+Thanks to purity and fast-and-loose reasoning,
+we have exactly one way to implement this function.
+
+The only way to get a `b` is to apply an `a` to `a -> Result b a`.
+If the result of application is `Ok a`,
+we still don't have a `b` and must apply this new `a` to `a -> Result b a`.
+At some point we get a value `Err b`, and thus we can finally end the recursion.
+
+You might be wondering, "Why is this modeled with `Result b a`?"
+Let's think about how else we could model this function.
+We want to know if we should continue recursing or if we should stop.
+We could introduce our own type like:
+
+```elm
+type Step a b
+  = Stop a
+  | Recurse b
+```
+
+The type of `recurse` would then become `(a -> Step b a) -> a -> b`.
+The implementation would be almost exactly the same save for some renaming.
+So, why not make this move?
+
+One reason not to make this move is because it forces an additional API onto the use of this function.
+Everybody that uses `recurse` has to now learn about `Step a b`.
+They have to learn what value constructors exist.
+They have to learn what functions operate on the type.
+That seems like a bit too much to ask a user of this function.
+Until elm figures out a story for shared vocabulary that doesn't involve convention,
+we'll always have this trade-off to consider.
+
+Another reason not to make this move is because it forces us to maintain a type that is isomorphic to `Result a b`.
+Any function we could think to write either already has been written for `Result a b`,
+or it should be written and pushed to the `Result` module, the `Result.Extra` module, or some other central location.
+Which begs the question as to whether this [`recurse`][recurse] function deserves its own module or not...
+In any case, as maintainers, we get absolutely no benefit from adding `Step a b`.
+
+Rather than force those burdens, we chose to use `Result a b`.
+the user doesn't have to learn anything new and we don't have to maintain `ResultJr a b`.
 
 ## Example
 
@@ -185,15 +233,21 @@ Now if we apply `1000000` to `even`, we'll actually get a value, rather than a r
 If you have a non-tail recursive function,
 use this function to ensure it won't blow the stack.
 
-Some explanation for how this works.
+```elm
+collatz : Int -> Result Int Int
+collatz n =
+  if n == 1 then
+    Err 1
+  else if n % 2 == 0 then
+    Ok (n // 2)
+  else
+    Ok (3 * n + 1)
 
-Thanks to purity and fast-and-loose reasoning,
-we have exactly one way to implement this function.
 
-The only way to get a `b` is to apply an `a` to `a -> Result b a`.
-If the result of application is `Ok a`,
-we still don't have a `b` and must apply this new `a` to `a -> Result b a`.
-At some point we get a value `Err b`, and thus we can finally end the recursion.
+conjecture : Int -> Int
+conjecture =
+  recurse collatz
+```
 -}
 recurse : (a -> Result b a) -> a -> b
 recurse f x =
